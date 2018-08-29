fa68882c7318b6733cf101f1b93a8d2c9d36f1df
Husny Ahamed <gazzali@omobio.net>
2018-08-29 12:44:02 +0530

    default path set

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/test", (request, response) => {
+app.get("/", (request, response) => {
   response.send("Hello from Express!");
 });
 
